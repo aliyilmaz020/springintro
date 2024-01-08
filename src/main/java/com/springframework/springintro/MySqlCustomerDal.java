86cb1252d9157dcb31ddc1a0d6d1149f72c65047
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-09 00:56:13 +0300

    Metinsel değerlerin injectionu

--- src/main/java/com/springframework/springintro/MySqlCustomerDal.java
+++ src/main/java/com/springframework/springintro/MySqlCustomerDal.java
@@ -2,8 +2,19 @@ package com.springframework.springintro;
 
 public class MySqlCustomerDal implements ICustomerDal {
 
+  String connectionString;
+
+  public String getConnectionString() {
+    return connectionString;
+  }
+
+  public void setConnectionString(String connectionString) {
+    this.connectionString = connectionString;
+  }
+
   @Override
   public void add() {
+    System.out.println("Connection String : " + this.connectionString);
     System.out.println("MySql Eklendi");
   }
 
